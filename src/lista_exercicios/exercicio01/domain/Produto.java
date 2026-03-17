f26729c46af7dc2fa19c8853fe7f81b85f703b02
Thomas Batista <139007458+thomasbatista@users.noreply.github.com>
2026-03-17 19:56:23 -0300

    ex01

---
+++ src/lista_exercicios/exercicio01/domain/Produto.java
@@ -0,0 +1,41 @@
+package lista_exercicios.exercicio01.domain;
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private double preco;
+
+    public Produto(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto: " + "código: " + codigo + ", nome: " + nome + ", preço: " + preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
---
+++ src/lista_exercicios/exercicio01/test/ProdutoTest01.java
@@ -0,0 +1,18 @@
+package lista_exercicios.exercicio01.test;
+
+import lista_exercicios.exercicio01.domain.Produto;
+
+public class ProdutoTest01 {
+    static void main() {
+        Produto produto = new Produto(123, "Teclado");
+
+        System.out.println("-- Dados iniciais --");
+        System.out.println(produto);
+
+        produto.setNome("Mouse");
+        produto.setPreco(100);
+
+        System.out.println("-- Dados atualizados --");
+        System.out.println(produto);
+    }
+}
